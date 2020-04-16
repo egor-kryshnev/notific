f1e961f76a2455b1f666d5ad02095d9c98ef1982
Yagor Kryshniov <egork@aplit-soft.com>
2020-04-16 14:52:11 +0300

    work heroku

--- angular-push-notifications/src/app/push-notification.service.ts
+++ angular-push-notifications/src/app/push-notification.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 // const SERVER_URL = 'http://localhost:3000/subscription';
-const SERVER_URL = 'http://192.168.1.13:3000/subscription';
+// const SERVER_URL = 'http://192.168.1.13:3000/subscription';
+const SERVER_URL = 'https://notification-angular.herokuapp.com/subscription';
 
 @Injectable({
   providedIn: 'root'
